a99023d55974259169b4586afb4a74568101d16b
Alex Marriott <marriottaog@gmail.com>
2021-09-20 17:10:02 +0100

    seed file now only searches for first id to limit search size

--- db/seed.js
+++ db/seed.js
@@ -2,7 +2,12 @@ const faker = require('faker');
 const Vehicle = require('../models/vehicle');
 
 const seedVehicles = () => {
-  Vehicle.findAll().then((vehicles) => {
+  Vehicle.findAll({
+    where: {
+      id: 1
+    }
+  })
+  .then((vehicles) => {
     if(!vehicles[0]) {
       for (let i = 0; i < 10; i++) {
         Vehicle.create({make: faker.vehicle.manufacturer(), model: faker.vehicle.model()});
