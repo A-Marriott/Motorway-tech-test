c130707408b12a3eb33c811d87188a0bcf527f6d
Alex Marriott <marriottaog@gmail.com>
2021-09-20 14:38:26 +0100

    init sqlite database

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const db = require('./config/database')
+
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.get('/', (req, res) => {
   res.json({test: 'hello'})
 });
