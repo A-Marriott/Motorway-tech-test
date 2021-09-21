4bd715312af604979205907e70462c9679438603
Alex Marriott <marriottaog@gmail.com>
2021-09-21 10:08:42 +0100

    added catch for router

--- router.js
+++ router.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const vehicleRouter = require('./controller/vehicles');
 
-router.get('/api/vehicles', (req, res) => {
-  vehicleRouter.getVehicle().then((vehicles) => res.json(vehicles));
+router.get('/api/vehicles', (req, res, next) => {
+  vehicleRouter.getVehicle()
+  .then((vehicles) => res.json(vehicles))
+  .catch(next)
 });
 
 module.exports = router;
